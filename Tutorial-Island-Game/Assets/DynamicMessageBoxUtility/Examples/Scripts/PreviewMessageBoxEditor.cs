a0f4887f97485d2b5adc96175b742c60dd5e177b
Ilmari Tarpila <leo.tarpila@aalto.fi>
2018-01-27 07:27:19 +0200

    Looking into shaking rock animation

---
+++ Tutorial-Island-Game/Assets/DynamicMessageBoxUtility/Examples/Scripts/PreviewMessageBoxEditor.cs
@@ -0,0 +1,30 @@
+﻿using CustomMessageBoxes;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof (StylePreviewer), true)]
+public class PreviewMessageBoxEditor : Editor {
+
+		private StylePreviewer script;
+
+		public void OnEnable()
+		{
+			script = (StylePreviewer) target;
+		}
+
+	public override void OnInspectorGUI(){
+		DrawDefaultInspector();
+
+		EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+		if (GUILayout.Button("Preview"))
+		{
+			MessageBoxCreator.CreateMessageBox (script.style, script.customTitle, script.customMessage, script.customButtonText);
+			EditorGUI.EndDisabledGroup();
+		}
+
+		if (GUILayout.Button ("Delete all")) 
+		{
+			MessageBoxCreator.DestroyAllMessageBoxes();
+		}
+	}
+}
---
+++ Tutorial-Island-Game/Assets/DynamicMessageBoxUtility/Examples/Scripts/StylePreviewer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using CustomMessageBoxes;
+
+class StylePreviewer : MonoBehaviour
+{
+    public MessageStyle style;
+    public string customTitle;
+    public string customMessage;
+    public string customButtonText;
+
+    void Start()
+    { 
+        MessageBoxCreator.CreateMessageBox(style);
+    }
+}
+
